931b6c28086447e33f51dcccf0ce7cd183c25fa3
den <d.n.voropaev@gmail.com>
2021-01-05 23:32:33 +0300

    added a way to store pgp key in a system keychain

--- src/scrambler.ts
+++ src/scrambler.ts
@@ -1,8 +1,11 @@
 import * as openpgp from 'openpgp'
-import { randomBytes } from 'crypto'
+import { randomBytes, createHash } from 'crypto'
 import { networkInterfaces } from 'os'
+import * as keytar from 'keytar'
 
-type ThenArg<T> = T extends PromiseLike<infer U> ? U : T
+const service = require ('../package.json').name
+
+type ResolvedType<T> = T extends PromiseLike<infer U> ? U : T
 
 export async function createKeys ({
     name,
@@ -14,25 +17,25 @@ export async function createKeys ({
     email: string,
     passphrase: string,
     numBits?: number
-}): Promise<ThenArg<ReturnType<typeof openpgp.generateKey>> & {
-    mac: string,
+}): Promise<ResolvedType<ReturnType<typeof openpgp.generateKey>> & {
+    tag: string,
     email: string,
     passphrase: string,
-    revoke: () => ReturnType<typeof openpgp.revokeKey>
 }> {
+
     const interfaces = networkInterfaces ()
-    let mac: string
+    let tag: string
 
     for (const name in interfaces) {
         if (name !== 'en0' && name !== 'eth0') continue
         for (const info of interfaces[name]) {
             if (!info.mac) continue
-            mac = info.mac
+            tag = createHash ('sha256').update (info.mac).digest ('hex')
             break
         }
     }
 
-    if (!mac) {
+    if (!tag) {
         throw new Error ('Unable to determine MAC address')
     }
 
@@ -40,20 +43,47 @@ export async function createKeys ({
         passphrase = randomBytes (256).toString ('base64')
     }
 
-    const email = useremail.split('@').map((w, i) => i === 0 ? `${w}+${mac.replace(/:/g, '_')}` : w).join('@')
+    const email = useremail.split ('@').map ((w, i) => i === 0 ? `${w}+${tag}` : w).join ('@')
     const key = await openpgp.generateKey ({
         userIds: [{ name, email }],
         numBits,
         passphrase
     })
 
-    const revoke = async () => {
-        return openpgp.revokeKey ({
-            // @ts-ignore
-            key: (await openpgp.key.readArmored (key.publicKeyArmored)).keys[0],
-            revocationCertificate: key.revocationCertificate
-        })
-    }
+    return { ...key, tag, email, passphrase }
+}
 
-    return Object.assign (key, {mac, email, passphrase, revoke })
+export async function revokeKey ({
+    publicKeyArmored,
+    privateKeyArmored,
+    revocationCertificate,
+}: ResolvedType<ReturnType<typeof openpgp.generateKey>>): Promise<ResolvedType<ReturnType<typeof openpgp.revokeKey>>> {
+    return openpgp.revokeKey ({
+        // @ts-ignore: the typings are a bit outdated
+        key: (await openpgp.key.readArmored (publicKeyArmored)).keys[0],
+        revocationCertificate
+    })
+}
+
+export function storeKeys ({
+    publicKeyArmored,
+    privateKeyArmored,
+    revocationCertificate,
+    account,
+}: ResolvedType<ReturnType<typeof openpgp.generateKey>> & {
+    account: string
+}): ReturnType<typeof keytar.setPassword> {
+
+    const payload = JSON.stringify ({ publicKeyArmored, privateKeyArmored, revocationCertificate })
+    return keytar.setPassword (service, account, payload)
+}
+
+export function deleteKeys ({ account }: { account: string }): ReturnType<typeof keytar.deletePassword> {
+    return keytar.deletePassword (service, account)
+}
+
+export async function getKeys ({ account }: { account: string }): Promise<ResolvedType<ReturnType<typeof openpgp.generateKey>>>{
+
+    const keys = await keytar.getPassword (service, account)
+    return JSON.parse (keys) as ResolvedType<ReturnType<typeof openpgp.generateKey>>
 }
--- tests/scrambler.ts
+++ tests/scrambler.ts
@@ -1,16 +1,45 @@
-import { createKeys } from '../src/scrambler'
+import { expect } from 'chai'
+import {
+    createKeys,
+    revokeKey,
+    getKeys,
+    storeKeys,
+    deleteKeys,
+} from '../src/scrambler'
 
 describe ('scrambler', function () {
 
     this.timeout(5000)
 
+    const email = 'test@test.com'
+
     it ('generates a new keypair', async () => {
         const key = await createKeys ({
             name: 'Test',
-            email: 'test@test.com',
+            email,
             passphrase: 'Test test test test test test'
         })
 
-        await key.revoke ()
+        await revokeKey (key)
+    })
+
+    it ('stores a key in the keychain', async () => {
+        const key = await createKeys ({
+            name: 'Test',
+            email,
+            passphrase: 'Test test test test test test'
+        })
+
+        const account = key.email
+
+        await storeKeys ({ ...key, account })
+        const keys = await getKeys ({ account })
+
+        expect (keys).to.have.property ('publicKeyArmored', key.publicKeyArmored)
+        expect (keys).to.have.property ('privateKeyArmored', key.privateKeyArmored)
+        expect (keys).to.have.property ('revocationCertificate', key.revocationCertificate)
+
+        await deleteKeys ({ account })
+        await revokeKey (key)
     })
 })
